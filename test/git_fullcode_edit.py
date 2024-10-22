be17930af5afdedd74f9148b7843059b5c236f54
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-10-22 12:43:32 +0900

    Update git_fullcode_edit.py

--- test/git_fullcode_edit.py
+++ test/git_fullcode_edit.py
@@ -1,6 +1,6 @@
 #model dir 
 #C:\{$username}\noway\AppData\Local\nomic.ai\GPT4All\Meta-Llama-3-8B-Instruct.Q4_0.gguf
 #from gpt4all import GPT4All
-#model_dir = r'C:\<your_username>\noway\AppData\Local\nomic.ai\GPT4All\Meta-Llama-3-8B-Instruct.Q4_0.gguf'
+#model_dir = r'C:\<your_username>\AppData\Local\nomic.ai\GPT4All\Meta-Llama-3-8B-Instruct.Q4_0.gguf'
 #model = GPT4All(model_dir)
 

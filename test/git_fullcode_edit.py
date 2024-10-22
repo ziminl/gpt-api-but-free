5e8ea8c4ada317c7300ed70aba48a21e6217d796
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-10-22 16:04:51 +0900

    Update git_fullcode_edit.py

--- test/git_fullcode_edit.py
+++ test/git_fullcode_edit.py
@@ -4,3 +4,18 @@
 #model_dir = r'C:\<your_username>\AppData\Local\nomic.ai\GPT4All\Meta-Llama-3-8B-Instruct.Q4_0.gguf'
 #model = GPT4All(model_dir)
 
+#file to text
+#py js html css
+#csv
+#db
+#parquet
+
+#rather put in localdocs
+#docx
+
+#png jpg jpeg
+#not good to import and need to find way
+#ipynb
+#pptx
+
+
